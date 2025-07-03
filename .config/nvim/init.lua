afe06edcf7d01d43c3ef9f1fef96da6f365836a2
Neoriat <Neoriat@duck.com>
2025-07-03 18:53:19 +0330

    Reconfiguriong nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,10 +4,3 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 require("config.lazy")
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
-
