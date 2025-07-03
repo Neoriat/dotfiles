92a23eb1e9b9194dbcc61cc480fe8e9b26939ac3
Neoriat <Neoriat@duck.com>
2025-07-03 18:56:53 +0330

    New configuration

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+local opt = vim.opt
+
+opt.expandtab = true    -- Use spaces instead of tab characters
+opt.tabstop = 4         -- Show tabs as 4 spaces wide
+opt.shiftwidth = 4      -- Indent/outdent operations use 4 spaces
+opt.softtabstop = 4     -- Inserts/deletes up to 4 spaces per <Tab>/<BS>
+
