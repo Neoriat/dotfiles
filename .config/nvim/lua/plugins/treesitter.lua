b997d1321915a61f90dbe4cc2470ab1a17865ad6
Neoriat <Neoriat@duck.com>
2025-07-03 18:58:10 +0330

    Configured treesitter.nvim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,32 @@
-return {{
-    "nvim-treesitter/nvim-treesitter",
+return {
+    "nvim-treesitter/nvim-treesitter", 
+    branch = 'master',
+    lazy = false,
     build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "c", "lua", "python", "javascript", "html", "json", "bash", "typescript", "css", "rust"
+        },
+        sync_install = false,      -- install parsers async
+        auto_install = true,       -- auto-install missing parsers on BufRead
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,           -- experimental indentation
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+      })
+    end,
+  } 
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "html", "css", "python", "java", "kotlin", "json", "rust" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
- }}
