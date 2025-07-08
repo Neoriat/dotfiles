c3e3c56d0ee52444f2f2057e1c72edfed395bbbe
Neoriat <Neoriat@duck.com>
2025-07-08 20:18:00 +0330

    blink now uses lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,7 @@ vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+require("config.lazy")
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
@@ -17,6 +18,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+if vim.fn.has("nvim-0.11") == 1 and vim.lsp.config then
+  vim.lsp.config("*", {
+    capabilities = require("blink.cmp").get_lsp_capabilities(),
+  })
+end
+
 vim.lsp.enable({ "lua_ls", "pyright", "ts_ls", "rust-analyzer"})
 
-require("config.lazy")
